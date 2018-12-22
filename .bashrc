d93b34137bd86f157a2695ac7f91aeb409eab5a5
youfa.song <vsyfar@gmail.com>
2018-12-23 00:41:16 +0800

    init

---
+++ .bashrc
@@ -0,0 +1,14 @@
+
+
+###################### grep ##########################
+# for grep
+GREP_OPTIONS+="--exclude-dir=\.svn --exclude-dir=\.git --exclude=tags --exclude=cscope\.out"
+GREP_OPTIONS+=" --color=auto"
+alias grep="grep $GREP_OPTIONS"
+#export GREP_COLOR='1;32'
+
+######################################################
+
+
+
+export PATH=~/dotfiles/bin:$PATH
---
+++ install.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+#
+# install.sh
+# Copyright (C) 2018 youfa.song <vsyfar@gmail.com>
+#
+# Distributed under terms of the GPLv2 license.
+#
+
+
+#export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export DOTFILES_DIR="$PWD"
+ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
+
+#echo ". $DOTFILES_DIR/.bashrc" >> ~/.bashrc
