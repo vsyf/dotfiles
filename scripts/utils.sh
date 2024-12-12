967e5fe3cc0f33d1be20c3fcdb5ffd8c6ed0a42d
youfa <vsyfar@gmail.com>
2024-12-12 21:27:38 +0800

    normal update dot files
    Signed-off-by: youfa <vsyfar@gmail.com>

---
+++ scripts/utils.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+#
+# utils.sh
+# Copyright (C) 2024 youfa <vsyfar@gmail.com>
+#
+# Distributed under terms of the GPLv2 license.
+#
+
+
+DOTFILES_DIR=$(cd "$(dirname "$0")"/.. && pwd)
+export DOTFILES_DIR
